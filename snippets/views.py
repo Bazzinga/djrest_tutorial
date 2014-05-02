c4640111451b4a668f5422f76977c255dcbcae61
gilsondev <contato@gilsondev.com>
2014-05-02 03:33:16 -0300

    Move coding to request and response branch

--- snippets/views.py
+++ snippets/views.py
@@ -1,3 +1,63 @@
-from django.shortcuts import render
+# -*- coding: utf-8 -*-
 
-# Create your views here.
+from django.http import HttpResponse
+from django.views.decorators.csrf import csrf_exempt
+from rest_framework import status
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+
+from snippets.models import Snippets
+from snippets.serializers import SnippetSerializer
+
+
+@api_view(['GET', 'POST'])
+def snippet_list(request, format=None):
+    """
+    List all code snippets, or create a new snippet.
+    """
+    if request.method == 'GET':
+        snippets = Snippets.objects.all()
+        serializer = SnippetSerializer(snippets, many=True)
+        return Response(serializer.data)
+
+    if request.method == 'POST':
+        serializer = SnippetSerializer(data=request.DATA)
+        if serializer.is_valid():
+            serializer.save()
+
+            # Status 201: The request has been fulfilled and resulted
+            # in a new resource being created.
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+        # Status 400: The request can not be delivered
+        # due to incorrect syntax.
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+@api_view(['GET', 'PUT', 'DELETE'])
+def snippet_detail(request, pk, format=None):
+    """
+    Retrieve, update or delete a code snippet.
+    """
+    try:
+        snippet = Snippets.objects.get(pk=pk)
+    except Snippets.DoesNotExist:
+        # Status 404: Not Found
+        return Response(status=status.HTTP_404_NOT_FOUND)
+
+    if request.method == 'GET':
+        serializer = SnippetSerializer(snippet)
+        return Response(serializer.data)
+
+    elif request.method == 'PUT':
+        serializer = SnippetSerializer(snippet, data=request.DATA)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    elif request.method == 'DELETE':
+        snippet.delete()
+        # Status 204: The server successfully processed the request,
+        # but is not returning any content.
+        return Response(status=status.HTTP_204_NO_CONTENT)
